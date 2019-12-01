5e050988c2026753a8ee0518e5e074aadc6cd983
Amber Taranto <ambzzz@gmail.com>
2019-11-30 20:12:38 -0800

    Done.

---
+++ db/migrate/20191201040643_add_association_columns.rb
@@ -0,0 +1,9 @@
+class AddAssociationColumns < ActiveRecord::Migration[5.0]
+  def change
+    change_table :rides do |t|
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+    end
+
+  end
+end
---
+++ vendor/bundle/gems/ffi-1.11.3/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/agomez/code/join-the-fun-rails-online-web-pt-061019/vendor/bundle/gems/ffi-1.11.3/ext/ffi_c/libffi/src/x86/ffitarget.h
